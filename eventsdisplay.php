9ab812c139ac62630d4bb46f155fd0bba8f5d200
Paul <paulmerlyn@sbcglobal.net>
2013-03-31 20:36:40 -0700

    Update eventsdisplay.php

--- eventsdisplay.php
+++ eventsdisplay.php
@@ -25,7 +25,7 @@ if (empty($_POST['selectedID']))
 	}
 
 // Connect to DB
-$db = mysql_connect('localhost', 'paulme6_merlyn', 'fePhaCj64mkik')
+$db = mysql_connect('localhost', 'paulme6_merlyn', '')
 or die('Could not connect: ' . mysql_error());
 mysql_select_db('paulme6_medtrainers') or die('Could not select database: ' . mysql_error());
 
