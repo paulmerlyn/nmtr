7e9fbc20b0eda20575450751a355413015042c03
Paul <paulmerlyn@sbcglobal.net>
2013-03-31 20:23:38 -0700

    Update addtrainer.php

--- addtrainer.php
+++ addtrainer.php
@@ -10,7 +10,7 @@ session_start();
 ob_start();
 
 // Connect to DB
-$db = mysql_connect('localhost', 'paulme6_merlyn', 'fePhaCj64mkik')
+$db = mysql_connect('localhost', 'paulme6_merlyn', '')
 or die('Could not connect: ' . mysql_error());
 mysql_select_db('paulme6_medtrainers') or die('Could not select database: ' . mysql_error());
 ?>
